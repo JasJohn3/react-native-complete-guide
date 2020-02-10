b0630608fe8a03007d2246f988307bf7ed3d0ba4
Jason Lane Johnson <jasjohn3@uat.edu>
2020-02-10 14:58:28 -0700

    Missed Change for MODAL

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import { StyleSheet,  View, Button,FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 export default function App() {
@@ -7,6 +7,8 @@ export default function App() {
   //CourseGoals, setCourseGoals are a series of arrays that will store previous user input values into a list.
   const [courseGoals, setCourseGoals] = useState([]);
 
+  //A 
+  const [isAddMode, setIsAddMode] = useState(false);
   //event handler that takes the previous course goal listed as the variable courseGoals and stores them into an array.
   //The Array uses the spread operator to store the previous goals, than adds the entered goals to the end of the list.
   const addGoalHandler = goalTitle =>
@@ -14,16 +16,22 @@ export default function App() {
     setCourseGoals(currentGoals =>[...currentGoals,
       {id: Math.random().toString(), value: goalTitle}]);
   }
+  //A function which searches for the ID of an element and removes the element if the ID matches the Input ID from the user
   const removeGoalHandler = goalID =>{
     setCourseGoals(currentGoals =>{
       return currentGoals.filter((goal) => goal.id !== goalID);
     })
   }
+  // const setIsAddMode = () =>
+  // {
+  //   return true;
+  // }
     
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler}/>
+      <Button title="Add New Objective" onPress={() =>setIsAddMode(true)}/>
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler}/>
       <View>
         <FlatList keyExtractor={(item, index)=> item.id} data = {courseGoals} renderItem = {itemData => <GoalItem id = {itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />} />
     </View>
